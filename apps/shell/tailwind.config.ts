34a0af29efe0f9f041a5c46661ee06702f33b7b6
Yelyzaveta Havrylenko <yelyzaveta.havrylenko@exaring.de>
2025-11-17 16:40:34 +0100

    Remove tailwind from shell

--- apps/shell/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
-    "../../libs/**/*.{astro,html,js,jsx,ts,tsx,vue}", // include shared libs
-  ],
-  theme: { extend: {} },
-} satisfies Config;
